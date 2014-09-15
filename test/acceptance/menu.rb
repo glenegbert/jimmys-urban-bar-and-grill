37de95ac0a9ce499c9bf7d257d9a0b6219b15c32
Ephemere <objectivparadigm@gmail.com>
2014-09-15 14:06:04 -0600

    wi[

--- test/acceptance/menu.rb
+++ test/acceptance/menu.rb
@@ -3,6 +3,10 @@ require_relative '../test_helper'
 describe "menu" do
   include Capybara::DSL
 
+  def teardown
+    Capybara.reset_sessions!
+  end
+
   it "visits menu page" do
     visit '/menu'
     assert page.has_content?("Menu")
